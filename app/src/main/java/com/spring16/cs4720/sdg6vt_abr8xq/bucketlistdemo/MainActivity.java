5282c6518f3bbbc6cde6b513bc8001f070a0033e
AlexRamey <alex.b.ramey@gmail.com>
2016-01-30 13:39:01 -0500

    Initial Commit. Empty Project.

---
+++ app/src/main/java/com/spring16/cs4720/sdg6vt_abr8xq/bucketlistdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.spring16.cs4720.sdg6vt_abr8xq.bucketlistdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/spring16/cs4720/sdg6vt_abr8xq/bucketlistdemo/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.spring16.cs4720.sdg6vt_abr8xq.bucketlistdemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
