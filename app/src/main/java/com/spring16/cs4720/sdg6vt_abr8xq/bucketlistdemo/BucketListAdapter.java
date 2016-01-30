fc560ab1ce969c454870b34b0bbad68e00fde142
AlexRamey <alex.b.ramey@gmail.com>
2016-01-30 16:02:09 -0500

    Set up recycler view with adapter and linear layout manager.

---
+++ app/src/main/java/com/spring16/cs4720/sdg6vt_abr8xq/bucketlistdemo/BucketListAdapter.java
@@ -0,0 +1,58 @@
+package com.spring16.cs4720.sdg6vt_abr8xq.bucketlistdemo;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+/**
+ * Created by alexramey on 1/30/16.
+ */
+public class BucketListAdapter extends RecyclerView.Adapter<BucketListAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    // Wrapper for the cell
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mNumberView;
+        public TextView mNameView;
+        public ToggleButton mToggleButton;
+        public ViewHolder(View v) {
+            super(v);
+            mNumberView = (TextView)(v.findViewById(R.id.item_number));
+            mNameView = (TextView)(v.findViewById(R.id.item_name));
+            mToggleButton = (ToggleButton)(v.findViewById(R.id.item_toggle));
+        }
+    }
+
+    // constructor
+    public BucketListAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public BucketListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_bucket_list_cell, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mNumberView.setText(String.valueOf(position));
+        holder.mNameView.setText(mDataset[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+
+}
--- app/src/main/java/com/spring16/cs4720/sdg6vt_abr8xq/bucketlistdemo/BucketListFragment.java
+++ app/src/main/java/com/spring16/cs4720/sdg6vt_abr8xq/bucketlistdemo/BucketListFragment.java
@@ -1,11 +1,15 @@
 package com.spring16.cs4720.sdg6vt_abr8xq.bucketlistdemo;
 
 
+import android.app.usage.NetworkStats;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 
 
 /**
@@ -13,17 +17,35 @@ import android.view.ViewGroup;
  */
 public class BucketListFragment extends Fragment {
 
+    private RecyclerView mBucketListRecyclerView;
+    private RecyclerView.LayoutManager mBucketListLayoutManager;
+    private BucketListAdapter mBucketListAdapter;
 
     public BucketListFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bucket_list, container, false);
+        View root = inflater.inflate(R.layout.fragment_bucket_list, container, false);
+
+        mBucketListRecyclerView = (RecyclerView)root.findViewById(R.id.bucket_list_recycler_view);
+
+        // Boosts performance by guaranteeing that all the cells are the same size
+        mBucketListRecyclerView.setHasFixedSize(true);
+
+        // Set recycler view layout manager
+        mBucketListLayoutManager = new LinearLayoutManager(this.getActivity());
+        mBucketListRecyclerView.setLayoutManager(mBucketListLayoutManager);
+
+        // Set recycler view adapter
+        String[] names = {"Shake the hand of Dean Groves!", "Streak the Lawn!", "Arrest Sheriff"};
+        mBucketListAdapter = new BucketListAdapter(names);
+        mBucketListRecyclerView.setAdapter(mBucketListAdapter);
+
+        return root;
     }
 
 }
